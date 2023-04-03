672ef32273aca5a9953ee046363049381c6781f3
admindinge <admin@dinge>
2023-04-03 22:41:23 +0400

    aaa

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Warehouse
+
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'company')
+    list_filter = ('company',)
+    search_fields = ('name', 'address', 'company__name')
+
+admin.site.register(Warehouse, WarehouseAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Warehouse(models.Model):
+    warehouse_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    remark = models.TextField(blank=True)
+    remark1 = models.TextField(blank=True)
+    
+def __str__(self):
+    return self.name
+
+# модель для товаров
+# class Product(models.Model):
+#     product_id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=255)
+        
+    
+    # сделать связь с моделью склада
+    # сделать связь с моделью компании
+    # сделать связь с моделью категории
